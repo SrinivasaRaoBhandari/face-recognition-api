2452fa726a056de76ae383f4450b15a094f0a592
Srinivas <bhandarisrinivasarao@gmail.com>
2020-09-13 22:07:20 +0530

    modifying files

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.get('/',(req,res)=>{
-	res.send(database.users)
+	res.send('it is working!')
 })
 
 app.post('/signin', (req,res) => {signin.handleSignin(req, res, db, bcrypt)})
@@ -41,6 +41,6 @@ app.put('/image', (req,res) => {image.handleImage(req,res,db)})
 
 app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
 
-app.listen(3000,()=>{
-	console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000,()=>{
+	console.log("app is running on port ${process.env.PORT}");
 })
